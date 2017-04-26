71c8efef5bd5e39321876cb2b093489cb40d4aa8
sofiapardillo <topaz1197@gmail.com>
2017-04-26 11:38:49 -0400

    First Commit

---
+++ CodingFin/CodingFin.ino
@@ -0,0 +1,30 @@
+const int a = 13;
+const int b = 12;
+const int c = 11;
+const int d = 10;
+const int e = 9;
+const int f = 8;
+const int g = 7;
+const int h = 6;
+const int i = 5;
+const int j = 4;
+
+int voltageInput = A0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int volts = analogRead(voltageInput);
+  Serial.println(volts);
+
+ if (volts >){
+
+  
+ }
+
+ if (volts >){
+  
+ }
+}
