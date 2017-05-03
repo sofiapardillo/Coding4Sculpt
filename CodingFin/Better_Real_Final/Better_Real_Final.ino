9bdd55d81fb3d9e4e7df87764038ee5f74595bb2
sofiapardillo <topaz1197@gmail.com>
2017-05-03 01:53:33 -0400

    Commit Fin

--- CodingFin/Better_Real_Final/Better_Real_Final.ino
+++ CodingFin/Better_Real_Final/Better_Real_Final.ino
@@ -1,4 +1,3 @@
-
 const int XWHITE3 = 13;
 const int PURPLE2 = 12;
 const int WHITE5 = 11;
@@ -47,7 +46,7 @@ void loop() {
 
 //////First circut
 
-if (FirstVolt < 5 ){
+if (FirstVolt < 10 ){
 CircutOne = int(random(2,4));
 
 if (CircutOne == 2){
@@ -114,12 +113,11 @@ if (brightness <= 0 || brightness >= 150){
   }
  
  }
- 
  }
 
 ////First Circut Reset
 
- if (FirstVolt > 5 ){
+ if (FirstVolt > 10 ){
     analogWrite(PURPLE1,0);
     analogWrite(PURPLE2,0);
  
@@ -138,97 +136,84 @@ if (brightness <= 0 || brightness >= 150){
 
 /////Second Circut
 
- if (SecondVolt < 5){
+ if (SecondVolt < 10){
   CircutTwo = int(random(2,5));
 
   if (CircutTwo == 2){
-    analogWrite(PURPLE1,255);
-    analogWrite(PURPLE2,255);
+    analogWrite(PURPLE1,random(150,255));
+    analogWrite(PURPLE2,random(150,255));
  
-    analogWrite(WHITE1,brightness);
-    analogWrite(WHITE2,brightness);
-    analogWrite(WHITE3,brightness);
-    analogWrite(WHITE4,brightness);
-    analogWrite(WHITE5,brightness);
-    analogWrite(WHITE6,brightness);
+    analogWrite(WHITE1,200);
+    analogWrite(WHITE2,200);
+    analogWrite(WHITE3,200);
+    analogWrite(WHITE4,0);
+    analogWrite(WHITE5,0);
+    analogWrite(WHITE6,0);
     
     analogWrite(XWHITE1,255);
     analogWrite(XWHITE2,255);
     analogWrite(XWHITE3,255);
-
-    brightness = brightness + fadeAmount;
-     if (brightness <= 0 || brightness >= 100){
-      fadeAmount = -fadeAmount;
-     }
-     delay (100);
   }
  
   if (CircutTwo == 3){
-    analogWrite(PURPLE1,200);
-    analogWrite(PURPLE2,200);
+    analogWrite(PURPLE1,brightness);
+    analogWrite(PURPLE2,brightness);
  
-    analogWrite(WHITE1,brightness);
-    analogWrite(WHITE2,brightness);
-    analogWrite(WHITE3,brightness);
-    analogWrite(WHITE4,brightness);
-    analogWrite(WHITE5,brightness);
-    analogWrite(WHITE6,brightness);
+    analogWrite(WHITE1, random(150,255));
+    analogWrite(WHITE2,0);
+    analogWrite(WHITE3,random(150,255));
+    analogWrite(WHITE4,0);
+    analogWrite(WHITE5,random(150,255));
+    analogWrite(WHITE6,0);
     
-    analogWrite(XWHITE1,255);
-    analogWrite(XWHITE2,255);
-    analogWrite(XWHITE3,255);
+    analogWrite(XWHITE1,brightness);
+    analogWrite(XWHITE2,0);
+    analogWrite(XWHITE3,brightness);
 
-    brightness = brightness + fadeAmount;
-     if (brightness <= 0 || brightness >= 255){
+     brightness = brightness + fadeAmount;
+     if (brightness <= 0 || brightness >= 150){
       fadeAmount = -fadeAmount;
      }
-     delay (500);
+     delay (100);
   }
 
    if (CircutTwo == 4){
     analogWrite(PURPLE1,0);
     analogWrite(PURPLE2,0);
  
-    analogWrite(WHITE1,brightness);
-    analogWrite(WHITE2,brightness);
-    analogWrite(WHITE3,brightness);
-    analogWrite(WHITE4,brightness);
-    analogWrite(WHITE5,brightness);
-    analogWrite(WHITE6,brightness);
+    analogWrite(WHITE1,255);
+    analogWrite(WHITE2,255);
+    analogWrite(WHITE3,255);
+    analogWrite(WHITE4,255);
+    analogWrite(WHITE5,255);
+    analogWrite(WHITE6,255);
     
-    analogWrite(XWHITE1,200);
-    analogWrite(XWHITE2,200);
-    analogWrite(XWHITE3,200);
-
-    brightness = brightness + fadeAmount;
-     if (brightness <= 0 || brightness >= 200){
-      fadeAmount = -fadeAmount;
-     }
-     delay (300);
+    analogWrite(XWHITE1,0);
+    analogWrite(XWHITE2,0);
+    analogWrite(XWHITE3,0);
   }
 
    if (CircutTwo == 5){
-    analogWrite(PURPLE1,200);
-    analogWrite(PURPLE2,200);
+    analogWrite(PURPLE1,random(150,255));
+    analogWrite(PURPLE2,random(150,255));
  
     analogWrite(WHITE1,0);
     analogWrite(WHITE2,0);
-    analogWrite(WHITE3,0);
-    analogWrite(WHITE4,0);
+    analogWrite(WHITE3,255);
+    analogWrite(WHITE4,255);
     analogWrite(WHITE5,0);
     analogWrite(WHITE6,0);
     
-    analogWrite(XWHITE1,random(0,255));
-    analogWrite(XWHITE2,random(0,255));
-    analogWrite(XWHITE3,random(0,255));
+    analogWrite(XWHITE1,random(150,255));
+    analogWrite(XWHITE2,255);
+    analogWrite(XWHITE3,255);
   }
-  delay (500);
+ 
  }
-
 
  ////Second Circut Reset
 
-  if (SecondVolt > 5){
+  if (SecondVolt > 10 ){
     analogWrite(PURPLE1,0);
     analogWrite(PURPLE2,0);
  
@@ -248,25 +233,62 @@ if (brightness <= 0 || brightness >= 150){
  //Third Circut
 
  if (ThirdVolt < 10){
+  CircutThree = int(random(2,4));
 
-  for (in = 0; in < 6.283; in = in + 0.0015)
-  {
-    out = sin(in) * 127.5 + 127.5;
-
-    analogWrite(PURPLE1,random(0,255));
-    analogWrite(PURPLE2,out);
+  if (CircutThree == 2){
+    analogWrite(PURPLE1,255);
+    analogWrite(PURPLE2,255);
  
-    analogWrite(WHITE1,random(0,255));
-    analogWrite(WHITE2,out);
-    analogWrite(WHITE3,random(0,255));
-    analogWrite(WHITE4,out);
-    analogWrite(WHITE5,random(0,255));
-    analogWrite(WHITE6,out);
+    analogWrite(WHITE1,brightness);
+    analogWrite(WHITE2,brightness);
+    analogWrite(WHITE3,brightness);
+    analogWrite(WHITE4,brightness);
+    analogWrite(WHITE5,brightness);
+    analogWrite(WHITE6,brightness);
     
-    analogWrite(XWHITE1,random(0,255));
-    analogWrite(XWHITE2,out);
-    analogWrite(XWHITE3,random(0,255));
+    analogWrite(XWHITE1,255);
+    analogWrite(XWHITE2,255);
+    analogWrite(XWHITE3,255);
+
+     brightness = brightness + fadeAmount;
+     if (brightness <= 0 || brightness >= 150){
+      fadeAmount = -fadeAmount;
+     }
+     delay (100);
   }
+ 
+  if (CircutThree == 3){
+    analogWrite(PURPLE1,0);
+    analogWrite(PURPLE2,0);
+ 
+    analogWrite(WHITE1,255);
+    analogWrite(WHITE2,255);
+    analogWrite(WHITE3,255);
+    analogWrite(WHITE4,255);
+    analogWrite(WHITE5,255);
+    analogWrite(WHITE6,255);
+    
+    analogWrite(XWHITE1,0);
+    analogWrite(XWHITE2,0);
+    analogWrite(XWHITE3,0);
+  }
+
+   if (CircutThree == 4){
+    analogWrite(PURPLE1,255);
+    analogWrite(PURPLE2,255);
+ 
+    analogWrite(WHITE1,0);
+    analogWrite(WHITE2,0);
+    analogWrite(WHITE3,0);
+    analogWrite(WHITE4,0);
+    analogWrite(WHITE5,0);
+    analogWrite(WHITE6,0);
+    
+    analogWrite(XWHITE1,255);
+    analogWrite(XWHITE2,255);
+    analogWrite(XWHITE3,255);
+  }
+ 
  }
 
 ////Third Circut Reset
@@ -291,55 +313,59 @@ if (brightness <= 0 || brightness >= 150){
  ///Fourth Circut
 
  if (FourthVolt < 10){
-  CircutFour = int(random(2,3));
+  CircutFour = int(random(2,4));
 
   if (CircutFour == 2){
-
-    for (in = 0; in < 6.283; in = in + 0.0015)
-  {
-    out = sin(in) * 127.5 + 127.5;
-
-    analogWrite(PURPLE1,out);
-    analogWrite(PURPLE2,out);
+    analogWrite(PURPLE1,255);
+    analogWrite(PURPLE2,255);
  
-    analogWrite(WHITE1,200);
-    analogWrite(WHITE2,200);
-    analogWrite(WHITE3,200);
-    analogWrite(WHITE4,200);
-    analogWrite(WHITE5,200);
-    analogWrite(WHITE6,200);
+    analogWrite(WHITE1,0);
+    analogWrite(WHITE2,0);
+    analogWrite(WHITE3,0);
+    analogWrite(WHITE4,0);
+    analogWrite(WHITE5,0);
+    analogWrite(WHITE6,0);
     
     analogWrite(XWHITE1,255);
     analogWrite(XWHITE2,255);
     analogWrite(XWHITE3,255);
-    
-   
-  }
-  delay (150);
- 
+
+    delay(300);
   }
  
   if (CircutFour == 3){
-
-       for (in = 0; in < 6.283; in = in + 0.0015)
-  {
-    out = sin(in) * 127.5 + 127.5;
+    analogWrite(PURPLE1,255);
+    analogWrite(PURPLE2,255);
+ 
+    analogWrite(WHITE1,random(150,255));
+    analogWrite(WHITE2,random(150,255));
+    analogWrite(WHITE3,random(150,255));
+    analogWrite(WHITE4,random(150,255));
+    analogWrite(WHITE5,random(150,255));
+    analogWrite(WHITE6,random(150,255));
     
-    analogWrite(PURPLE1,out);
+    analogWrite(XWHITE1,255);
+    analogWrite(XWHITE2,255);
+    analogWrite(XWHITE3,255);
+  }
+
+   if (CircutFour == 4){
+    analogWrite(PURPLE1,255);
     analogWrite(PURPLE2,255);
  
     analogWrite(WHITE1,0);
-    analogWrite(WHITE2,255);
-    analogWrite(WHITE3,out);
-    analogWrite(WHITE4,255);
+    analogWrite(WHITE2,0);
+    analogWrite(WHITE3,0);
+    analogWrite(WHITE4,0);
     analogWrite(WHITE5,0);
-    analogWrite(WHITE6,out);
+    analogWrite(WHITE6,0);
     
-    analogWrite(XWHITE1,0);
-    analogWrite(XWHITE2,0);
-    analogWrite(XWHITE3,0);
-  }}
+    analogWrite(XWHITE1,255);
+    analogWrite(XWHITE2,255);
+    analogWrite(XWHITE3,255);
 
+    delay (700);
+  }
  }
 
  ///Fourth Circut Reset
